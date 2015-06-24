80697d00bd3be6067518a9be89a3af6dd88d5f7a
xhdhr10000 <xhdhr2007@126.com>
2015-06-24 17:45:24 +0800

    code 6.24

--- js/edit.js
+++ js/edit.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     editResize();
     $(window).resize(editResize);
+    $("#asubmit").click(submit);
 });
 
 function editResize() {
@@ -12,5 +13,25 @@ function editResize() {
         "top": h.height(),
         "width": w.width()*0.8,
         "height": w.height() - h.height() - 30
+    });
+}
+
+function submit() {
+    if ($("#ititle").val().length == 0) {
+        alert("请输入文章标题");
+        return;
+    }
+
+    $.post("/php/article.php?f=s", {
+        u: getCookie("c_username"),
+        t: $("#ititle").val(),
+        k: $("#ikeyword").val(),
+        a: encodeURI(CKEDITOR.instances.tcontent.getData())
+    },
+    function(data, status) {
+        if (status == "success") {
+            if (data.search("error") != -1) alert(data);
+            else window.top.location.href = "/html/article.html?id="+data;
+        }
     });
 }
